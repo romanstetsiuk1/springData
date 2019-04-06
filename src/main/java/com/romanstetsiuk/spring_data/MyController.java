7edac393001a3d3e62d05752207d87f10fefbb68
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-06 14:18:08 +0200

    Create Product

---
+++ src/main/java/com/romanstetsiuk/spring_data/MyController.java
@@ -0,0 +1,7 @@
+package com.romanstetsiuk.spring_data;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+}
--- src/main/java/com/romanstetsiuk/spring_data/SpringDataApplication.java
+++ src/main/java/com/romanstetsiuk/spring_data/SpringDataApplication.java
@@ -1,13 +1,34 @@
 package com.romanstetsiuk.spring_data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+    @Autowired
+    ProductRepository productRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Product product = new Product();
+        product.setName("Snickers");
+        product.setDescription("chocolate");
+
+        productRepository.save(product);
+
+        Optional<Product> product1 = productRepository.findProductByName("Snickers");
+
+        product1.ifPresent(System.out::println);
+
+    }
 }
